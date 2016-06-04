e5e2466ef9ecae7803a4ab7fdc2675d08cd3d358
豪山本 <goyamamoto1998@gmail.com>
2016-06-04 15:29:17 +0900

    initial commit

--- count20160604/ViewController.swift
+++ count20160604/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+      override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+    }
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+    }
+    @IBAction func clear(){
+        number = 0
+        label.text = String(number)
+    }
+    @IBAction func kakeru(){
+        number = number * 2
+        label.text = String(number)
+    }
+    @IBAction func waru(){
+        number = number/2
+        label.text=String(number)
 }
 
